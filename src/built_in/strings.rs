ba1dec602e323c0b0be027dccc252c7f8532b4b2
MyselfLeo <myselfleo@outlook.fr>
2022-09-12 15:32:36 +0200

    /

--- src/built_in/strings.rs
+++ src/built_in/strings.rs
@@ -189,9 +189,9 @@ pub fn expect_positive_index(value: Option<Value>, limit: Option<usize>) -> Resu
 
 
 fn to_num(scope: Rc<RefCell<Scope>>, program: &mut SlothProgram) -> Result<(), Error> {
-    let value = scope.borrow().get_variable("@self".to_string(), program).unwrap();
+    let value = super::get_self(&scope, program)?;
 
-    let result = match value.borrow().to_owned() {
+    let result = match value {
         Value::String(x) => {
             match x.parse::<f64>() {
                 Ok(v) => Value::Number(v),
@@ -218,9 +218,9 @@ fn to_num(scope: Rc<RefCell<Scope>>, program: &mut SlothProgram) -> Result<(), E
 
 
 fn len(scope: Rc<RefCell<Scope>>, program: &mut SlothProgram) -> Result<(), Error> {
-    let value = scope.borrow().get_variable("@self".to_string(), program).unwrap();
+    let value = super::get_self(&scope, program)?;
 
-    let result = match value.borrow().to_owned() {
+    let result = match value {
         Value::String(x) => Value::Number(x.len() as f64),
         _ => panic!("Implementation of method 'len' for type 'string' was called on a value of another type")
     };
@@ -238,7 +238,6 @@ fn len(scope: Rc<RefCell<Scope>>, program: &mut SlothProgram) -> Result<(), Erro
 
 
 fn insert(scope: Rc<RefCell<Scope>>, program: &mut SlothProgram) -> Result<(), Error> {
-
     let inputs = super::query_inputs(&scope, vec![Type::Number, Type::String], "insert")?;
 
     let self_value = super::get_self(&scope, program)?;
@@ -247,17 +246,16 @@ fn insert(scope: Rc<RefCell<Scope>>, program: &mut SlothProgram) -> Result<(), E
         _ => panic!("Implementation of method 'insert' for type 'string' was called on a value of another type")
     };
 
+
     let idx = super::expect_natural(inputs[0].clone(), Some((string.len(), "length of string")), 1)?;
     
-
     let insertion = match &inputs[1] {
         Value::String(v) => v,
         _ => panic!()
     };
-
-
     string.insert_str(idx, insertion);
     
+
     super::set_self(&scope, program, Value::String(string))
 }
 
@@ -266,43 +264,25 @@ fn insert(scope: Rc<RefCell<Scope>>, program: &mut SlothProgram) -> Result<(), E
 
 
 fn push(scope: Rc<RefCell<Scope>>, program: &mut SlothProgram) -> Result<(), Error> {
-    let scope_borrow = scope.borrow();
+    let inputs = super::query_inputs(&scope, vec![Type::String], "push")?;
 
-    let owner_v = scope_borrow.get_variable("@self".to_string(), program).unwrap();
-    let inputs = scope_borrow.get_inputs();
-
-    if inputs.len() != 1 {
-        let err_msg = format!("Called function 'push' with {} argument(s), but the function requires 1 arguments", inputs.len());
-        return Err(Error::new(ErrorMessage::InvalidArguments(err_msg), None));
-    }
-
-
-    let mut string = match owner_v.borrow().to_owned() {
+    let self_value = super::get_self(&scope, program)?;
+    let mut string = match self_value {
         Value::String(x) => x,
-        _ => panic!("Implementation of method 'insert' for type 'string' was called on a value of another type")
+        _ => panic!("Implementation of method 'push' for type 'string' was called on a value of another type")
     };
 
 
-    let insert_value = match inputs[0].borrow().to_owned() {
+    let insert_value = match inputs[0] {
         Value::String(x) => x,
-        v => {
-            let err_msg = format!("Argument 1 of function 'push' is of type string, given a value of type {}", v.get_type());
-            return Err(Error::new(ErrorMessage::InvalidArguments(err_msg), None));
-        }
+        _ => panic!()
     };
     
+
     string.push_str(&insert_value);
     
     // try to edit owner value
-    let res = match owner_v.try_borrow_mut() {
-        Ok(mut borrow) => {
-            *borrow = Value::String(string);
-            Ok(())
-        },
-        Err(e) => return Err(Error::new(ErrorMessage::RustError(e.to_string()), None))
-    };
-
-    res
+    super::set_self(&scope, program, Value::String(string))
 }
 
 
@@ -312,36 +292,19 @@ fn push(scope: Rc<RefCell<Scope>>, program: &mut SlothProgram) -> Result<(), Err
 
 
 fn remove(scope: Rc<RefCell<Scope>>, program: &mut SlothProgram) -> Result<(), Error> {
-    let scope_borrow = scope.borrow();
-
-    let owner_v = scope_borrow.get_variable("@self".to_string(), program).unwrap();
-    let inputs = scope_borrow.get_inputs();
-
-    if inputs.len() != 1 {
-        let err_msg = format!("Called function 'remove' with {} argument(s), but the function requires 1 arguments", inputs.len());
-        return Err(Error::new(ErrorMessage::InvalidArguments(err_msg), None));
-    }
+    let owner_v = super::get_self(&scope, program)?;
+    let inputs = super::query_inputs(&scope, vec![Type::Number], "remove")?;
 
 
-    let mut string = match owner_v.borrow().to_owned() {
+    let mut string = match owner_v {
         Value::String(x) => x,
-        _ => panic!("Implementation of method 'insert' for type 'string' was called on a value of another type")
+        _ => panic!("Implementation of method 'remove' for type 'string' was called on a value of another type")
     };
 
-    let idx = expect_positive_index(inputs.get(0).map(|v| v.borrow().to_owned()), Some(string.len() - 1))?;
-
+    let idx = super::expect_natural(inputs[0], Some((string.len(), "string length")), 0)?;
     string.remove(idx);
     
-    // try to edit owner value
-    let res = match owner_v.try_borrow_mut() {
-        Ok(mut borrow) => {
-            *borrow = Value::String(string);
-            Ok(())
-        },
-        Err(e) => return Err(Error::new(ErrorMessage::RustError(e.to_string()), None))
-    };
-
-    res
+    super::set_self(&scope, program, Value::String(string))
 }
 
 
@@ -381,7 +344,7 @@ fn split(scope: Rc<RefCell<Scope>>, program: &mut SlothProgram) -> Result<(), Er
 
     let vec = string.split(&sep).map(|x| Rc::new(RefCell::new(Value::String(x.to_string())))).collect();
 
-    super::set_return(scope, program, Value::List(Type::String, vec))?;
+    super::set_return(&scope, program, Value::List(Type::String, vec))?;
 
     Ok(())
 }
@@ -420,7 +383,7 @@ fn get(scope: Rc<RefCell<Scope>>, program: &mut SlothProgram) -> Result<(), Erro
         (string, idx)
     };
     
-    super::set_return(scope, program, Value::String(string.char_indices().nth(idx).unwrap().1.to_string()))?;
+    super::set_return(&scope, program, Value::String(string.char_indices().nth(idx).unwrap().1.to_string()))?;
 
     Ok(())
 }
