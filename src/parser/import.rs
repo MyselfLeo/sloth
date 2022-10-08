15d94356a507940f1e5b7efb23bcbefe2aeb18df
MyselfLeo <myselfleo@outlook.fr>
2022-10-08 13:05:45 +0200

    .

--- src/parser/import.rs
+++ src/parser/import.rs
@@ -1,7 +1,10 @@
+use std::path::PathBuf;
+use regex::Regex;
+
 use crate::lexer::{Token, TokenStream, Keyword, Separator};
 use crate::sloth::function::{CustomFunction, FunctionSignature};
 use crate::sloth::program::SlothProgram;
-use crate::errors::Error;
+use crate::errors::{Error, ErrMsg};
 use crate::sloth::statement::Statement;
 use crate::sloth::types::Type;
 
@@ -9,45 +12,29 @@ use crate::sloth::types::Type;
 
 
 /// Parse an "import" statement, i.e the import of another .slo file. Different from the "builtin" statement which '''imports''' builtin functions and structures
-pub fn parse_import(iterator: &mut TokenStream, program: &mut SlothProgram, warning: bool, origin_path: PathBuf) -> Result<(), Error> {
-    let first_pos = match iterator.current() {
-        Some((_, p)) => p.clone(),
-        None => return Err(eof_error(line!()))
-    };
-
-    // Next token is a literal string with the name of the file to import
-    let (path, last_pos) = match iterator.next() {
+pub fn parse_import(stream: &mut TokenStream, program: &mut SlothProgram, warning: bool, origin_path: PathBuf) -> Result<(), Error> {
+    // import keyword
+    let (_, first_pos) = super::expect_token(stream, Token::Keyword(Keyword::Import))?;
+    
+    // name of the file, as a literal (NOT an identifier)
+    let filename = match stream.current() {
         Some((Token::Literal(s), p)) => {
-            // get path from literal
-            match Value::from_raw_token(s.clone()) {
-                Value::String(_) => {
-                    // Cleans the literal, as it will have ' " 'before and after
-                    let mut name = s.strip_prefix("\"").unwrap();
-                    name = name.strip_suffix("\"").unwrap();
+            let re = Regex::new(r#"^"(.*)""#).unwrap();
+            let file_name = match re.captures(&s) {
+                Some(cap) => cap.get(1),
+                None => None,
+            };
 
-                    let working_dir = origin_path.parent().unwrap().to_path_buf();
-
-                    let mut file = working_dir.clone();
-                    file.push(name);
-
-                    if origin_path == file {
-                        let err_msg = format!("File '{}' imports itself", iterator.current().unwrap().1.filename);
-                        return Err(Error::new(ErrMsg::ImportError(err_msg), Some(p.clone())))
-                    }
-
-                    (file, p)
-                },
-                _ => {
-                    let err_msg = format!("Expected filename, got unexpected token '{}'", s);
-                    return Err(Error::new(ErrMsg::SyntaxError(err_msg), Some(p.clone())))
+            match file_name {
+                Some(v) => v.as_str().to_string(),
+                None => {
+                    let err_msg = format!("Expected filename, got '{}'", s);
+                    return Err(Error::new(ErrMsg::ImportError(err_msg), Some(p)))
                 }
             }
         },
-        Some((t, p)) => {
-            let err_msg = format!("Expected filename, got unexpected token '{}'", t.original_string());
-            return Err(Error::new(ErrMsg::SyntaxError(err_msg), Some(p.clone())))
-        },
-        None => return Err(eof_error(line!()))
+
+        o => return Err(super::wrong_token(o, "file name"))
     };
 
     // parse the file for the program
