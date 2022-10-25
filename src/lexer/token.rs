c35c41b8d11301b66e3cb8edefbc163772d2518d
MyselfLeo <myselfleo@outlook.fr>
2022-10-25 11:26:10 +0200

    removed commented code

--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -6,16 +6,6 @@ use super::operator::Operator;
 
 
 
-/*#[derive(Clone, Debug, PartialEq)]
-pub enum TokenType {
-    Keyword,
-    Separator,
-    Identifier,
-    Operator,
-    Literal
-}*/
-
-
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Token {
@@ -28,17 +18,6 @@ pub enum Token {
 
 
 impl Token {
-    /*pub fn get_type(&self) -> TokenType {
-        match self {
-            Token::Keyword(_) => TokenType::Keyword,
-            Token::Separator(_) => TokenType::Separator,
-            Token::Identifier(_) => TokenType::Identifier,
-            Token::Operator(_) => TokenType::Operator,
-            Token::Literal(_) => TokenType::Literal,
-        }
-    }*/
-
-
     /// Return the token corresponding to the given text. Will test for keyword, operator and separator.
     pub fn from_str(string: &str) -> Result<Token, String> {
         let identifier_re = Regex::new(r"^(@[0-9]+|@[a-zA-Z]+|[a-zA-Z_][a-zA-Z0-9_]*)$").unwrap();
