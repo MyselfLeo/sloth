202dd30feedec4ff918c2bba8aa22755aafb74a6
MyselfLeo <myselfleo@outlook.fr>
2022-11-24 14:07:20 +0100

    /

--- src/operations/add.rs
+++ src/operations/add.rs
@@ -12,15 +12,17 @@ pub fn get_all() -> Vec<OperatorFunction> {
     static NUM_NUM: fn(Vec<Value>) -> Value = num_num;
     static NUM_STRING: fn(Vec<Value>) -> Value = num_string;
     static STRING_STRING: fn(Vec<Value>) -> Value = string_string;
+    static SAME_LISTS: fn(Vec<Value>) -> Value = same_lists;
 
     vec![
         // Number and Strings
         OperatorFunction::new(Add, vec![Number, Number], Number, &NUM_NUM),
         OperatorFunction::new(Add, vec![Number, String], String, &NUM_STRING),
         OperatorFunction::new(Add, vec![String, Number], String, &NUM_STRING),
-        OperatorFunction::new(Add, vec![String, String], String, &STRING_STRING)
+        OperatorFunction::new(Add, vec![String, String], String, &STRING_STRING),
 
         // Lists
+        OperatorFunction::new(Add, vec![List(Box::new(Number)), List(Box::new(Number))], List(Box::new(Number)), &SAME_LISTS),
     ]
 }
 
@@ -43,6 +45,18 @@ fn num_string(v: Vec<Value>) -> Value {
 fn string_string(v: Vec<Value>) -> Value {
     if let (Value::String(x), Value::String(y)) = (&v[0], &v[1]) {
         Value::String(x.to_owned() + y)
+    }
+    else {unreachable!()}
+}
+
+
+fn same_lists(v: Vec<Value>) -> Value {
+    if let (Value::List(t1, mut v1), Value::List(t2, mut v2)) = (v[0].clone(), v[1].clone()) {
+        if t1 == t2 {
+            v1.append(&mut v2);
+            Value::List(t1.clone(), v1)
+        }
+        else {unreachable!()}
     }
     else {unreachable!()}
 }
