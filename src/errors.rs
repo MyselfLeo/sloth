d67b3f71309e05a4d942ee16406e96a85af6cad0
MyselfLeo <myselfleo@outlook.fr>
2022-12-03 15:54:21 +0100

    finished implementing backtraces

--- src/errors.rs
+++ src/errors.rs
@@ -1,10 +1,6 @@
 use crate::position::Position;
 
 
-const CRATE_NAME: &str = env!("CARGO_PKG_NAME");
-const CRATE_VERSION: &str = env!("CARGO_PKG_VERSION");
-
-
 #[derive(Debug, Clone)]
 pub enum ErrMsg {
     SyntaxError(String),
@@ -22,23 +18,42 @@ pub enum ErrMsg {
     FunctionError(String),
 }
 
+impl ErrMsg {
+    pub fn get_title(&self) -> String {
+        match self {
+            ErrMsg::SyntaxError(..) => "SYNTAX ERROR",
+            ErrMsg::NoEntryPoint(..) => "NO ENTRY POINT",
+            ErrMsg::FileError(..) => "FILE ERRO",
+            ErrMsg::InvalidArguments(..) => "INVALID ARGUMENTS",
+            ErrMsg::UnexpectedExpression(..) => "UNEXPECTED EXPRESSION",
+            ErrMsg::RuntimeError(..) => "RUNTIME ERROR",
+            ErrMsg::UnexpectedEOF(..) => "UNEXPECTED EOF",
+            ErrMsg::TypeError(..) => "TYPE ERROR",
+            ErrMsg::RustError(..) => "RUST ERROR",
+            ErrMsg::ReturnValueError(..) => "RETURN VALUE ERROR",
+            ErrMsg::ImportError(..) => "IMPORT ERROR",
+            ErrMsg::DefinitionError(..) => "DEFINITION ERROR",
+            ErrMsg::FunctionError(..) => "FUNCTION ERROR",
+        }.to_string()
+    }
+}
 
 impl std::fmt::Display for ErrMsg {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            ErrMsg::SyntaxError(e) => write!(f, "SYNTAX ERROR: {}", e),
-            ErrMsg::NoEntryPoint(e) => write!(f, "NO ENTRY POINT: {}", e),
-            ErrMsg::FileError(e) => write!(f, "FILE ERROR {}", e),
-            ErrMsg::InvalidArguments(e) => write!(f, "INVALID ARGUMENTS: {}", e),
-            ErrMsg::UnexpectedExpression(e) => write!(f, "UNEXPECTED EXPRESSION: {}", e),
-            ErrMsg::RuntimeError(e) => write!(f, "RUNTIME ERROR: {}", e),
-            ErrMsg::UnexpectedEOF(e) => write!(f, "UNEXPECTED EOF: {}", e),
-            ErrMsg::TypeError(e) => write!(f, "TYPE ERROR: {}", e),
-            ErrMsg::RustError(e) => write!(f, "RUST ERROR: {} (this error is from Rust, it is unlikely your fault)", e),
-            ErrMsg::ReturnValueError(e) => write!(f, "RETURN VALUE ERROR: {}", e),
-            ErrMsg::ImportError(e) => write!(f, "IMPORT ERROR: {}", e),
-            ErrMsg::DefinitionError(e) => write!(f, "DEFINITION ERROR: {}", e),
-            ErrMsg::FunctionError(e) => write!(f, "FUNCTION ERROR: {}", e),
+            ErrMsg::SyntaxError(e) => write!(f, "{}", e),
+            ErrMsg::NoEntryPoint(e) => write!(f, "{}", e),
+            ErrMsg::FileError(e) => write!(f, "{}", e),
+            ErrMsg::InvalidArguments(e) => write!(f, "{}", e),
+            ErrMsg::UnexpectedExpression(e) => write!(f, "{}", e),
+            ErrMsg::RuntimeError(e) => write!(f, "{}", e),
+            ErrMsg::UnexpectedEOF(e) => write!(f, "{}", e),
+            ErrMsg::TypeError(e) => write!(f, "{}", e),
+            ErrMsg::RustError(e) => write!(f, "{} (this error is from Rust, it is unlikely your fault)", e),
+            ErrMsg::ReturnValueError(e) => write!(f, "{}", e),
+            ErrMsg::ImportError(e) => write!(f, "{}", e),
+            ErrMsg::DefinitionError(e) => write!(f, "{}", e),
+            ErrMsg::FunctionError(e) => write!(f, "{}", e),
         }
     }
 }
@@ -67,18 +82,18 @@ impl Error {
     }
 
 
+    /// print the trace from the deeper to the shallower, then exit the program
     pub fn abort(&self) {
-        // print the trace from the deeper to the shallower
-
         // find the highest line error n° to determine the space required at the left of the backtrace line to fit line numbers
         let max_n = self.position_trace.iter()
                                                       .map(|p| p.line + 1)
                                                       .max();
 
-        let max_n = match max_n {
+        let mut max_n = match max_n {
             None => 0,
             Some(n) => n.to_string().len() + 2
         };
+        if max_n < 5 {max_n = 5}
 
 
         let mut raw_space = String::new();
@@ -89,7 +104,7 @@ impl Error {
             let file_string = std::fs::read_to_string(filepath).expect(format!("Unable to read file {:?}", filepath.as_os_str()).as_str());
             let lines: Vec<&str> = file_string.split('\n').collect();
 
-            if i == 0 {println!("\x1b[31m{raw_space}■ Error backtrace\x1b[90m {CRATE_NAME} v{CRATE_VERSION}\x1b[0m");}
+            if i == 0 {println!("\x1b[31m{raw_space}■ \x1b[91m{} (backtrace)\x1b[0m", self.message.get_title());}
             else {println!("\x1b[31m{raw_space}v\x1b[0m");}
             println!("\x1b[31m{raw_space}|\x1b[0m");
             
@@ -157,29 +172,43 @@ impl Warning {
     }
 
     pub fn warn(&self) {
+        let mut max_n = match &self.position {
+            None => 0,
+            Some(n) => n.line.to_string().len() + 2
+        };
+        if max_n < 5 {max_n = 5}
+
+        let mut raw_space = String::new();
+        for _ in 0..max_n {raw_space.push(' ')}
+
         match &self.position {
             None => println!("\x1b[93mWarning: {}\x1b[0m", self.text),
+
             Some(p) => {
                 let filepath = std::path::Path::new(&p.filename);
                 let file_string = std::fs::read_to_string(filepath).expect(format!("Unable to read file {:?}", filepath.as_os_str()).as_str());
                 let lines: Vec<&str> = file_string.split('\n').collect();
 
-                let line_index_str_len = (p.line + 1).to_string().len();
+                let nb_space_before_line_n = max_n - ((p.line + 1).to_string().len() + 1);
+
+                let mut spaces = String::new();
+                for _ in 0..nb_space_before_line_n {spaces.push(' ')}
 
                 println!("\x1b[93mWarning: {}\x1b[0m", self.text);
-                println!("\x1b[90m{}:{}  ({} v{})\x1b[0m", p.filename, p.line + 1, CRATE_NAME, CRATE_VERSION);
+                println!("\x1b[90m{}:{}\x1b[0m", p.filename, p.line + 1);
 
-                println!("\x1b[33m|\x1b[0m");
-                println!("\x1b[33m| {}\x1b[0m {}", p.line + 1, lines[p.line]);
-                print!("\x1b[33m| \x1b[93m");
-                for _ in 0..p.first_column + line_index_str_len + 1 {print!(" ")}
+                
+                println!("\x1b[33m{raw_space}|\x1b[0m");
+                println!("\x1b[33m{}{} | \x1b[0m {}", spaces, p.line + 1, lines[p.line]);
+                print!("\x1b[33m{raw_space}| \x1b[93m");
+                for _ in 0..p.first_column{print!(" ")}
                 
                 match p.last_column {
                     Some(n) => for _ in 0..(n - p.first_column + 1) {print!("^")},
                     None => for _ in 0..(lines[p.line].len() - p.first_column + 2) {print!("^")}
                 }
                 
-                println!("\x1b[0m");
+                println!("\x1b[0m\n");
             }
         }
     }
@@ -197,7 +226,6 @@ macro_rules! propagate {
         match $expr {
             Ok(v) => v,
             Err(e) => {
-                println!("Propagating from {}", line!());
                 return Err(e.with($err))
             }
         }
